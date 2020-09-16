592acac79a82907539188ddbafdf2a9a94f0417b
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-09-16 14:15:30 -0400

    Set init repo for project

---
+++ main.c
@@ -0,0 +1,6 @@
+#include "src/bigInt.h"
+
+int main()
+{
+	return 0;
+}
---
+++ src/bigInt.c
@@ -0,0 +1,7 @@
+#include "src/bigInt.h"
+
+// Plan bigInt implementation with the following features
+//
+//
+//	- isinstance of Num. (+) etc...
+//	- Out to any base.
